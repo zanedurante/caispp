37257d16fd088db8fcbe97297294f9b0a070d0d0
root <root@DESKTOP-RI2Q220.localdomain>
2019-09-01 16:15:08 -0700

    Update repo location in setup.py

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setuptools.setup(
     description="High level ML library used in CAIS++ Curriculum",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/pypa/caispp",
+    url="https://github.com/zanedurante/caispp",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
